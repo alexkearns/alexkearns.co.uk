010559286cc77d0e5bc8f9cb94004dd09c921a5a
Alex Kearns <alex@alexkearns.co.uk>
2022-09-23 18:30:57 +0100

    Update projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,50 +1,26 @@
 import Image from 'next/future/image'
 import Head from 'next/head'
 
+import {
+  GitHubIcon
+} from '@/components/SocialIcons'
+
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
 
 const projects = [
   {
-    name: 'Planetaria',
-    description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
+    name: 'DynamoDB Queryable Item History',
+    description: 'An example of how to implement an easily queryable DynamoDB item history store using S3, Glue Crawlers and Athena.',
+    link: { href: 'https://github.com/alexkearns/aws-dynamodb-item-history', label: 'github.com' },
+    logo: GitHubIcon,
   },
   {
-    name: 'Animaginary',
-    description:
-      'High performance web animation library, hand-written in optimized WASM.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
-  },
-  {
-    name: 'HelioStream',
-    description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
-  },
-  {
-    name: 'cosmOS',
-    description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
-  },
+    name: 'Implementing S3 to Snowflake via Snowpipe with Terraform',
+    description: 'Creates an end-to-end Snowpipe demo with S3 as a source using Terraform as the Infrastructure as Code tool.',
+    link: { href: 'https://github.com/alexkearns/snowpipe-terraform-demo', label: 'github.com' },
+    logo: GitHubIcon,
+  }
 ]
 
 function LinkIcon(props) {
@@ -62,37 +38,32 @@ export default function Projects() {
   return (
     <>
       <Head>
-        <title>Projects - Spencer Sharp</title>
+        <title>Projects - Alex Kearns</title>
         <meta
           name="description"
-          content="Things I’ve made trying to put my dent in the universe."
+          content="Bite-sized pieces of tech goodness that's free to adapt and learn from."
         />
       </Head>
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        title="Bite-sized pieces of tech goodness that's free to adapt and learn from."
+        intro="Just like many people that write software, I've worked on numerous little side projects that have never seen the light of day - these are the ones that I've deemed fit for human consumption!"
       >
         <ul
           role="list"
           className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
         >
-          {projects.map((project) => (
-            <Card as="li" key={project.name}>
-              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                <Image
-                  src={project.logo}
-                  alt=""
-                  className="h-8 w-8"
-                  unoptimized
-                />
+          {projects.map(({name, logo: Logo, description, link}) => (
+            <Card as="li" key={name}>
+              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-500 dark:ring-0">
+                <Logo />
               </div>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={project.link.href}>{project.name}</Card.Link>
+                <Card.Link href={link.href}>{name}</Card.Link>
               </h2>
-              <Card.Description>{project.description}</Card.Description>
+              <Card.Description>{description}</Card.Description>
               <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2">{project.link.label}</span>
+                <span className="ml-2">{link.label}</span>
               </p>
             </Card>
           ))}
