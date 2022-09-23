a0a2c0648dec4186db003f35a4516a218bc1ea18
Alex Kearns <alex@alexkearns.co.uk>
2022-09-23 19:15:24 +0100

    Add professional achievements

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -84,6 +84,19 @@ function ArrowDownIcon(props) {
   )
 }
 
+function ArrowTopRightOnSquare(props) {
+  return (
+    <svg fill="none" viewBox="0 0 24 24" aria-hidden="true" {...props}>
+        <path 
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5" 
+          d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" 
+        />
+    </svg>
+  )
+}
+
 function Article({ article }) {
   return (
     <Card as="article">
@@ -133,6 +146,78 @@ function Newsletter() {
         </Button>
       </div>
     </form>
+  )
+}
+
+function Achievements() {
+  let achievements = [
+    {
+      issuer: 'Amazon Web Services',
+      title: 'AWS Ambassador',
+      logo: logoPlanetaria,
+      start: '2022',
+      end: {
+        label: 'Present',
+        dateTime: new Date().getFullYear(),
+      },
+    },
+    {
+      issuer: 'Amazon Web Services',
+      title: 'AWS Community Builder',
+      logo: logoPlanetaria,
+      start: '2022',
+      end: {
+        label: 'Present',
+        dateTime: new Date().getFullYear(),
+      },
+    }
+  ]
+
+  return (
+    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <BriefcaseIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Professional Achievements</span>
+      </h2>
+      <ol className="mt-6 space-y-4">
+        {achievements.map((achievement, achievementIndex) => (
+          <li key={achievementIndex} className="flex gap-4">
+            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <Image src={achievement.logo} alt="" className="h-7 w-7" unoptimized />
+            </div>
+            <dl className="flex flex-auto flex-wrap gap-x-2">
+              <dt className="sr-only">Title</dt>
+              <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                {achievement.title}
+              </dd>
+              <dt className="sr-only">Issuer</dt>
+              <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+                {achievement.issuer}
+              </dd>
+              <dt className="sr-only">Date</dt>
+              <dd
+                className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+                aria-label={`${achievement.start.label ?? achievement.start} until ${
+                  achievement.end.label ?? achievement.end
+                }`}
+              >
+                <time dateTime={achievement.start.dateTime ?? achievement.start}>
+                  {achievement.start.label ?? achievement.start}
+                </time>{' '}
+                <span aria-hidden="true">—</span>{' '}
+                <time dateTime={achievement.end.dateTime ?? achievement.end}>
+                  {achievement.end.label ?? achievement.end}
+                </time>
+              </dd>
+            </dl>
+          </li>
+        ))}
+      </ol>
+      <Button href="https://www.credly.com/users/alexkearns/badges" variant="secondary" className="group mt-6 w-full">
+        View 5x AWS Certifications
+        <ArrowTopRightOnSquare className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+      </Button>
+    </div>
   )
 }
 
@@ -294,7 +379,7 @@ export default function Home({ articles }) {
             ))}
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
-            {/* <Newsletter /> */}
+            <Achievements />
             <Resume />
           </div>
         </div>
