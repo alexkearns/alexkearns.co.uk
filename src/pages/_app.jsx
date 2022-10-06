a2a3d14e2aa2eeef99abd7db511667b8b6dc780e
Alex Kearns <alex@alexkearns.co.uk>
2022-10-06 21:10:47 +0100

    Add Fathom integration

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react'
+import * as Fathom from 'fathom-client';
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
@@ -18,6 +19,28 @@ function usePrevious(value) {
 
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
+      includedDomains: ['www.alexkearns.co.uk', 'alexkearns.co.uk'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
 
   return (
     <>
