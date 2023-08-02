55dc1dbd0336494609f4fced42b1a6c7024cee61
Alex Kearns <alex@alexkearns.co.uk>
2023-08-02 21:47:09 +0100

    Add CloudFormation Fn::ForEach article

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,12 @@ module.exports = {
   singleQuote: true,
   semi: false,
   plugins: [require('prettier-plugin-tailwindcss')],
+  overrides: [
+    {
+      files: '*.mdx',
+      options: {
+        proseWrap: 'always',
+      },
+    },
+  ],
 }
